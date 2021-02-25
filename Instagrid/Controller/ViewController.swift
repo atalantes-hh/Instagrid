8af7c2b7bfe0c8f442a65cfc5c308cc4885b2905
Johann <johann.ulma@gmail.com>
2021-02-25 16:29:56 +0100

    Documentation
    Adding Documentation to the project

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+final class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // MARK: - Enumerations
     
